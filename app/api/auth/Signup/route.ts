ba45a6667dae2a5b5103b019921ea339923cec3e
luis juarez <luisjuarezcc9@gmail.com>
2023-12-21 09:26:17 -0600

    carpetasd de signup fueron creadas , lib para la conexion a la BD en mongo y validaciones en la ruta de signup

---
+++ app/api/auth/Signup/route.ts
@@ -0,0 +1,42 @@
+import { NextResponse } from "next/server";
+import User from "@/app/models/User";
+import bcrypt from "bcryptjs";
+
+export async function POST(request: Request) {
+//recibimos la informacion desde el cliente
+  const { username, email, password } = await request.json();
+
+  //validate password
+  if (!password || password.length < 8) {
+    return NextResponse.json(
+      { message: "Password must be at least 8 characters" },
+      { status: 400 }
+    );
+  }
+
+  //validar que el email y el usuario no exista
+  const emailFind = await User.findOne({ email });
+  const userFind =  await User.findOne({ username });
+
+  if (emailFind || userFind) {
+    return NextResponse.json(
+      { message: "Email or Username already exists" },
+      { status: 409 }
+    );
+  }
+
+  //encruptamos la password
+  const hasdPassword = await bcrypt.hash(password, 12);
+  //create user en caso de no existir
+  const user = new User({
+    email,
+    username,
+    password: hasdPassword,
+  });
+
+  const userSave = await user.save(); //guardamos el usuario en la base de datos
+  console.log(userSave);
+
+  console.log(username, email, password);
+  return NextResponse.json(userSave);
+}
--- app/models/User.js
+++ app/models/User.js
@@ -1,15 +1,33 @@
-import mongoose, { Schema } from "mongoose";
+import mongoose, { Schema, model, models } from "mongoose";
 
-//conexion a la base de datos
+// //conexion a la base de datos
 mongoose.connect(process.env.MONGODB_URI);
 mongoose.Promise = global.Promise; //usar promesas en mongoose 
 
 //creacion del esquema
-const userSchema= new Schema({
-    username:String,
-    email:String,
-    password:String,
+const userSchema = new Schema({
+    email:{
+        type:String,
+        unique:true,
+        required:[true, "El email es obligatorio"],
+        match:[/.+\@.+\..+/, "Por favor ingresa un email valido"]
+    },
+
+    username:{
+        type:String,
+        unique:true,
+        required:[true, "El username es obligatorio"],
+        minLength:[4, "El username debe tener al menos 4 caracteres"],
+        maxLength:[30, "El username debe tener menos de 16 caracteres"]
+    },
+
+    password:{
+        type:String,
+        required:[true, "El password es obligatorio"],
+        select:false
+    },
 
 });
+
 const User = mongoose.models.User || mongoose.model('User', userSchema); //si ya existe el modelo lo usa, si no lo crea
 export default User;
